2f0de4ab4c9fea34bca879e8c5767960720acc06
Valerian Hizkia <valhize@192.168.1.11>
2024-12-21 20:12:12 +0700

    Added buttons and css styling

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".box-container")
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -9,23 +9,34 @@ function getRandomColor() {
     return color;
 }
 
-for (let i = 0; i < 16; i++) {
+let amount = 16
+for (let i = 0; i < amount; i++) {
     const name = "abcdefghijklmnop"
     const horizontalContainer = document.createElement("div")
     horizontalContainer.setAttribute("style",
         "display: flex;\
         flex-direction: row;"
     )
-    for (let j = 0; j < 16; j++) {
+    for (let j = 0; j < amount; j++) {
         const div = document.createElement("div")
         horizontalContainer.appendChild(div)
         div.setAttribute(
             "style",
-            `background-color: ${getRandomColor()};\
-            width: 64px;\
-            height: 64px`
-        )
+            `width: ${512 / amount}px;\
+            height: ${512 / amount}px;
+            background: white;`)
         div.id = `${name[i]}${j}`
+        div.classList.add("boxes")
     }
     container.appendChild(horizontalContainer)
 }
+
+const boxes = document.querySelectorAll(".boxes")
+
+
+boxes.forEach(boxes => {
+    boxes.addEventListener("mouseenter",
+        (e) => {
+            e.target.style.background = "blue";
+        })
+});
