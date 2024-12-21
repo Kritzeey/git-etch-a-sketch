6e138175e5c4b76fae397f2269ea900835217641
Valerian Hizkia <valhize@192.168.1.11>
2024-12-21 20:57:15 +0700

    Moved grid making logic into makeGrid function and cleaned up syntax

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-const container = document.querySelector(".box-container")
-
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
     }
+
     return color;
 }
 
-let amount = 16
-for (let i = 0; i < amount; i++) {
-    const name = "abcdefghijklmnop"
-    const horizontalContainer = document.createElement("div")
-    horizontalContainer.setAttribute("style",
-        "display: flex;\
-        flex-direction: row;"
-    )
-    for (let j = 0; j < amount; j++) {
-        const div = document.createElement("div")
-        horizontalContainer.appendChild(div)
-        div.setAttribute(
-            "style",
-            `width: ${512 / amount}px;\
-            height: ${512 / amount}px;
-            background: white;`)
-        div.id = `${name[i]}${j}`
-        div.classList.add("boxes")
-    }
-    container.appendChild(horizontalContainer)
+function makeGrid(amount = 16) {
+    const container = document.querySelector(".box-container")
+
+    for (let i = 0; i < amount; i++) {
+        const name = "abcdefghijklmnop";
+        const horizontalContainer = document.createElement("div");
+
+        horizontalContainer.setAttribute("style",
+            "display: flex;\
+            flex-direction: row;"
+        );
+
+        for (let j = 0; j < amount; j++) {
+            const div = document.createElement("div")
+            horizontalContainer.appendChild(div)
+            div.setAttribute(
+                "style",
+                `width: ${512 / amount}px;\
+                height: ${512 / amount}px;
+                background: white;`)
+            div.id = `${name[i]}${j}`
+            div.classList.add("boxes")
+        };
+
+        container.appendChild(horizontalContainer);
+    };
+
+    const boxes = document.querySelectorAll(".boxes");
+
+    boxes.forEach(boxes => {
+        boxes.addEventListener("mouseenter",
+            (e) => {
+                e.target.style.background = "blue";
+            })
+    });
 }
 
-const boxes = document.querySelectorAll(".boxes")
-
-
-boxes.forEach(boxes => {
-    boxes.addEventListener("mouseenter",
-        (e) => {
-            e.target.style.background = "blue";
-        })
-});
+makeGrid()
